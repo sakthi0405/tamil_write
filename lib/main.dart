dd5788e94c971e3a31fe044f2cfba41441a75a62
Kanagasabapathy R <saba09102001@gmail.com>
2022-01-18 08:24:12 +0530

    initial-commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:signature_example/page/write_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Tamil Writer';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+             
+              tabs: [
+                Tab(text: 'Write'),
+                Tab(text: 'Verify'),
+              ],
+            ),
+            title: Text('Tamil Writer'),
+          ),
+          body: TabBarView(
+             physics: NeverScrollableScrollPhysics(),
+            children: [
+              WritePage(),
+              Icon(Icons.ac_unit),
+            ],
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+
+      //home: WritingPage(),
+    );
+  }
+}
