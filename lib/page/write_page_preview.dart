2cd286b87f35e152331db860ffc8d83d5b40e1ce
chitraarasu .k <kchitraarasu@gmail.com>
2022-01-20 09:42:02 +0530

    image format bug fixed

--- lib/page/write_page_preview.dart
+++ lib/page/write_page_preview.dart
@@ -2,12 +2,16 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:async';
+import 'dart:ui' as ui;
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
+
 class WritePreviewPage extends StatelessWidget {
   final Uint8List signature;
-
-  const WritePreviewPage({
+  static GlobalKey previewContainer = new GlobalKey();
+  WritePreviewPage({
     Key key,
     @required this.signature,
   }) : super(key: key);
@@ -27,40 +31,73 @@ class WritePreviewPage extends StatelessWidget {
             const SizedBox(width: 8),
           ],
         ),
-        body: Center(
-          child: Image.memory(signature, width: double.infinity),
+        body: RepaintBoundary(
+          key: previewContainer,
+          child: Center(
+            child: Image.memory(signature, width: double.infinity),
+          ),
         ),
       );
 
   Future storeSignature(BuildContext context) async {
-    final status = await Permission.storage.status;
-    if (!status.isGranted) {
-      await Permission.storage.request();
-    }
+    try {
+      RenderRepaintBoundary boundary =
+          previewContainer.currentContext.findRenderObject();
+      ui.Image image = await boundary.toImage();
+      final directory = (await getExternalStorageDirectory()).path;
+      print(directory);
 
-    final time = DateTime.now().toIso8601String().replaceAll('.', ':');
-    final name = 'tamil$time.png';
+      ByteData byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png);
+      Uint8List pngBytes = byteData.buffer.asUint8List();
+      print(pngBytes);
+      File imgFile = new File('$directory/screenshot.png');
 
-    final result = await ImageGallerySaver.saveImage(signature, name: name);
-    final isSuccess = result['isSuccess'];
-if (isSuccess) {
-      Navigator.pop(context);
-      ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(
-          "Saved",
-        ),
-        backgroundColor: Colors.green,
-      ));
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
+      imgFile
+          .writeAsBytes(pngBytes)
+          .then((value) => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  "Saved",
+                ),
+                backgroundColor: Colors.green,
+              )));
+    } catch (_) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text(
           'Failed to save',
         ),
         backgroundColor: Colors.red,
       ));
-    
     }
+
+//     final status = await Permission.storage.status;
+//     if (!status.isGranted) {
+//       await Permission.storage.request();
+//     }
+//
+//     final time = DateTime.now().toIso8601String().replaceAll('.', ':');
+//     final name = 'tamil$time.png';
+//
+//     final result = await ImageGallerySaver.saveImage(signature, name: name);
+//     final isSuccess = result['isSuccess'];
+// if (isSuccess) {
+//       Navigator.pop(context);
+//       ScaffoldMessenger.of(context).showSnackBar(
+//       SnackBar(
+//         content: Text(
+//           "Saved",
+//         ),
+//         backgroundColor: Colors.green,
+//       ));
+//     } else {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//       SnackBar(
+//         content: Text(
+//           'Failed to save',
+//         ),
+//         backgroundColor: Colors.red,
+//       ));
+//
+//     }
   }
 }
